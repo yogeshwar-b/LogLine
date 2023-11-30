5511fef38bf7a031e5c02250aecc79619e76c49c
Yogeshwar B <byogeshwarreddy@gmail.com>
2023-11-30 22:45:15 +0530

    Added a treeview to the activity panel

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const testView_1 = require("./LogLineVew");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -23,7 +24,7 @@ function activate(context) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from LogLine!');
 	});
-
+    new testView_1.TestView(context);
 	context.subscriptions.push(disposable);
 }
 
