16a7dee952f2ef78a0bccd7f85b498da6f036bbc
Yogeshwar B <byogeshwarreddy@gmail.com>
2023-12-03 00:22:12 +0530

    Added event handler for the button.

--- src/extension.js
+++ src/extension.js
@@ -1,22 +1,22 @@
-
-const vscode = require('vscode');
-const loglineview1= require('./loglineview');
+const vscode = require("vscode");
+const loglineview1 = require("./loglineview");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	const webview1provdier=new loglineview1.LogLineView()
-	let testdisposable=vscode.window.registerWebviewViewProvider("loglineview",webview1provdier);
-	context.subscriptions.push(testdisposable);
+  const webview1provdier = new loglineview1.LogLineView();
+  let disposable = vscode.window.registerWebviewViewProvider(
+    "loglineview",
+    webview1provdier
+  );
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
-
-
+  activate,
+  deactivate,
+};
--- src/loglineview.js
+++ src/loglineview.js
@@ -1,62 +1,65 @@
-const vscode=require('vscode');
+const vscode = require("vscode");
+
 /**
  * @implements {vscode.WebviewViewProvider}
  */
-class LogLineView{
-	/**
-	 * 
-	 * 
-	 */
-	constructor(
-	) { }
+class LogLineView {
+  /**
+   *
+   *
+   */
+  constructor() {}
 
-	/**
-	 * 
-	 * @param {vscode.WebviewView} webviewView 
-	 * @param {vscode.WebviewViewResolveContext} context 
-	 * @param {vscode.CancellationToken} _token 
-	 */
-	resolveWebviewView(
-		webviewView,
-		context ,
-		_token ,
-	) {
-		this._view = webviewView;
+  /**
+   *
+   * @param {vscode.WebviewView} webviewView
+   * @param {vscode.WebviewViewResolveContext} context
+   * @param {vscode.CancellationToken} _token
+   */
+  resolveWebviewView(webviewView, context, _token) {
+    this._view = webviewView;
 
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
 
-			localResourceRoots: [
-			]
-		};
+      localResourceRoots: [],
+    };
 
-		webviewView.webview.html = this._getHtmlForWebview();
-	}
+    webviewView.webview.html = this._getHtmlForWebview();
+    var count = 0;
+    // Handle messages from the webview
+    webviewView.webview.onDidReceiveMessage((message) => {
+      switch (message.command) {
+        case "alert":
+          vscode.window.showInformationMessage(message.text + count);
+          return;
+      }
+    }, undefined);
+  }
 
-	/**
-	 * 
-	 *@returns {string}
-	 */
-	_getHtmlForWebview() {
-		return `
+  /**
+   *
+   *@returns {string}
+   */
+  _getHtmlForWebview() {
+    return `
 		<html>
 		<body>
 			<h1>HELLO WORLD THIS IS ACTIVITY BAR!</h1>
 			<button id="myButton">Random Button</button>
 			<script>
+        const vscode = acquireVsCodeApi();
 				const button = document.getElementById('myButton');
 				button.addEventListener('click', () => {
-					// Perform actions when the button is clicked
-					// For example: send a message to the extension
-					console.log('Congratulations, your extension "logline" is now active!');
+          vscode.postMessage({command: "alert", text: "RANDOM BUTTON PRESSED!"});    
 				});
 			</script>
 		</body>
 		</html>`;
-	}
+  }
 }
 
-module.exports={
-    LogLineView
-}
+module.exports = {
+  LogLineView,
+};
